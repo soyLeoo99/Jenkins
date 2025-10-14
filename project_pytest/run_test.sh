5797b49d4022ce522e33947c55f9e4d22d701684
L e o <leoant08912@gmail.com>
2025-10-14 12:14:56 -0500

    Arrglando error 3

--- project_pytest/run_test.sh
+++ project_pytest/run_test.sh
@@ -1,9 +1,13 @@
 #! bin/bash
 
 echo "Activando entorno virtual"
+if[!"-d venv" ]; then
+    pytohn 3 -m venv venv
+fi
 source venv/bin/activate
 
 echo "Instalando dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Pruebas con pytest"
